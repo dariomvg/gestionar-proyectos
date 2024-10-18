fc7cfda1a76ac7499ad8ab207ef0324423ee9b0b
dariomvg <contact.dario.info@gmail.com>
2024-10-18 08:57:17 -0300

    fix css

--- src/components/CardProject.tsx
+++ src/components/CardProject.tsx
@@ -3,7 +3,6 @@ import { PropsCard } from "@/types/types";
 import { useRouter } from "next/navigation";
 import "../styles/cardProject.css"; 
 
-
 export const CardProject = ({ item }: PropsCard): JSX.Element => {
   const { title, description, date_limit, id } = item;
   const router = useRouter();
